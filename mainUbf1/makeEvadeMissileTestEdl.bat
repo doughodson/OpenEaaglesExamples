6b953be8aac8854dc797e5ef38e45540c6793a2d
Doug Hodson <doug@openeaagles.org>
2012-05-31 16:33:12 -0400

    update

--- mainUbf1/makeEvadeMissileTestEdl.bat
+++
@@ -1,1 +0,0 @@
-cl /E inputs\EvadeMissileTest.epp >test1.edl  /I ..\data /DFONT_PATH="\"../data/fonts/\""  /DFONT_11_19_PATH="\"../data/fonts/11x19/normal/\"" /DTEXTURES_PATH="\"../data/textures/\""
