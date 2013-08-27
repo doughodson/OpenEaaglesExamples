1b4a323892a86c883d4a2b843c1dbb2c160cd0f8
Jaeger <jaegerbomb23@gmail.com>
2013-08-27 07:39:19 -0400

    Addec climb, turn, and acceleration limit buttons for the dynamics model. Added a mode selector for the autopilot.

--- mainLaeroModel/MapDisplay.cpp
+++ mainLaeroModel/MapDisplay.cpp
@@ -5,6 +5,7 @@
 
 #include "openeaagles/vehicles/LaeroModel.h"
 #include "openeaagles/simulation/AirVehicle.h"
+#include "openeaagles/simulation/Autopilot.h"
 #include "openeaagles/simulation/Player.h"
 
 #include "openeaagles/basic/osg/Vec3"
@@ -32,14 +33,16 @@ MapDisplay::MapDisplay()
    startY = 0;
    dragging = false;
 
-   cmdAirspeed  = 200.0;
-   cmdAltitude  = 8000.0;
-   cmdHeading   = 0;
-
    cmdRangeSD.empty();
    cmdAirspeedSD.empty();
    cmdAltitudeSD.empty();
    cmdHeadingSD.empty();
+   apReqButtonVisSD.empty();
+   apModeSD.empty();
+   maxAccelSD.empty();
+   maxClimbSD.empty();
+   maxBankSD.empty();
+   maxTurnSD.empty();
 
    passiveEnable = false;
 }  
@@ -56,14 +59,16 @@ void MapDisplay::copyData(const MapDisplay& org, const bool cc)
    startY = org.startY;
    dragging = org.dragging;
 
-   cmdAirspeed  = org.cmdAirspeed;
-   cmdAltitude  = org.cmdAltitude;
-   cmdHeading   = org.cmdHeading;
-
    cmdRangeSD.empty();
    cmdAirspeedSD.empty();
    cmdAltitudeSD.empty();
    cmdHeadingSD.empty();
+   apReqButtonVisSD.empty();
+   apModeSD.empty();
+   maxAccelSD.empty();
+   maxClimbSD.empty();
+   maxBankSD.empty();
+   maxTurnSD.empty();
 
    passiveEnable = org.passiveEnable;
 }
@@ -92,37 +97,37 @@ void MapDisplay::mouseEvent(const int button, const int state, const int x, cons
       //std::cout << "LEFT_BUTTON = DRAGGING" << std::endl;
    }
 
-   if (button == GLUT_LEFT_BUTTON && state == GLUT_UP) {
-      int winX = glutGet(GLUT_WINDOW_X);
-      int winY = glutGet(GLUT_WINDOW_Y);
-      int winW = glutGet(GLUT_WINDOW_WIDTH);
-      int winH = glutGet(GLUT_WINDOW_HEIGHT);
+   //if (button == GLUT_LEFT_BUTTON && state == GLUT_UP) {
+   //   int winX = glutGet(GLUT_WINDOW_X);
+   //   int winY = glutGet(GLUT_WINDOW_Y);
+   //   int winW = glutGet(GLUT_WINDOW_WIDTH);
+   //   int winH = glutGet(GLUT_WINDOW_HEIGHT);
 
-      std::cout << "===========================================" << std::endl;
-      std::cout << "winX = " << std::setw(5) << winX << "    " << "winY = " << std::setw(5) << winY << std::endl;
-      std::cout << "winW = " << std::setw(5) << winW 
-                << "    "
-                << "winH = " << std::setw(5) << winH 
-                << std::endl;
+   //   std::cout << "===========================================" << std::endl;
+   //   std::cout << "winX = " << std::setw(5) << winX << "    " << "winY = " << std::setw(5) << winY << std::endl;
+   //   std::cout << "winW = " << std::setw(5) << winW 
+   //             << "    "
+   //             << "winH = " << std::setw(5) << winH 
+   //             << std::endl;
 
-      std::cout << "x    = " << std::setw(5) << x 
-                << "    "
-                << "y    = " << std::setw(5) << y 
-                << std::endl;
+   //   std::cout << "x    = " << std::setw(5) << x 
+   //             << "    "
+   //             << "y    = " << std::setw(5) << y 
+   //             << std::endl;
 
-      GLdouble l = 0, r = 0, t = 0, b = 0, n = 0, f = 0;
-      getOrtho(l, r, b, t, n, f);
-      std::cout << "l = " << std::setw(5) << l <<  "    " 
-                << "r = " << std::setw(5) << r << std::endl;
-      std::cout << "b = " << std::setw(5) << b <<  "    " 
-                << "t = " << std::setw(5) << t << std::endl;
-      std::cout << "n = " << std::setw(5) << n <<  "    " 
-                << "f = " << std::setw(5) << f << std::endl;
-   }
+   //   GLdouble l = 0, r = 0, t = 0, b = 0, n = 0, f = 0;
+   //   getOrtho(l, r, b, t, n, f);
+   //   std::cout << "l = " << std::setw(5) << l <<  "    " 
+   //             << "r = " << std::setw(5) << r << std::endl;
+   //   std::cout << "b = " << std::setw(5) << b <<  "    " 
+   //             << "t = " << std::setw(5) << t << std::endl;
+   //   std::cout << "n = " << std::setw(5) << n <<  "    " 
+   //             << "f = " << std::setw(5) << f << std::endl;
+   //}
 
-   if (button == GLUT_RIGHT_BUTTON && state == GLUT_UP) {
+   //if (button == GLUT_RIGHT_BUTTON && state == GLUT_UP) {
 
-   }
+   //}
 }
 
 // ----------------------------------------------------------------------------
@@ -137,12 +142,12 @@ void MapDisplay::passiveMotionEvent(const int x, const int y)
    }
 
    //std::cout << "passiveEnable = " << passiveEnable << std::endl;
-   if (passiveEnable) {
-      std::cout << "x = " << std::setw(5) << x 
-                << "    "
-                << "y = " << std::setw(5) << y 
-                << std::endl;
-   }
+   //if (passiveEnable) {
+   //   std::cout << "x = " << std::setw(5) << x 
+   //             << "    "
+   //             << "y = " << std::setw(5) << y 
+   //             << std::endl;
+   //}
 }
 
 // ----------------------------------------------------------------------------
@@ -176,101 +181,139 @@ void MapDisplay::mouseMotionEvent(const int x, const int y)
 // ----------------------------------------------------------------------------
 void MapDisplay::buttonEvent(const int b)
 {
-   //// cmdRange up, down
-   //MapPage* page = (MapPage*)(subpage());
+   // cmdRange up, down
+   MapPage* page = (MapPage*)(subpage());
 
-   //// cmdAirspeed, cmdAltitude, cmdHeading up, down
-   //Simulation::Aircraft* pA = getOwnship();
-   //Vehicle::LaeroModel* pR = 0;
-   //if (pA != 0) {
-   //   pR = (Vehicle::LaeroModel*)pA->getDynamicsModel();   
-   //}
-   ////MainDyn4Dof::LaeroModel* pR = getRacModel();
-   ////LaeroModel* pR = (LaeroModel*) getOwnship();
-
-
-   //if (page != 0 && pA != 0 && pR != 0) {
-
-   //   switch (b) { 
-   //      //-------------------------------------------------
-   //      // decrease range
-   //      //-------------------------------------------------
-   //      case 101:   if (page->getRange() > 5) {
-   //                     page->setRange(page->getRange() - 5);
-   //                     
-   //                  }
-   //                  break;
-
-   //      //-------------------------------------------------
-   //      // increase range
-   //      //-------------------------------------------------
-   //      case 102:   if (page->getRange() < 320) {
-   //                     page->setRange(page->getRange() + 5);
-   //                  }
-   //                  break;
-
-   //      //-------------------------------------------------
-   //      // decrease cmdAirspeed
-   //      //-------------------------------------------------
-   //      case 103:   if (cmdAirspeed > 100) {
-   //                     cmdAirspeed -= 10;
-   //                     pR->setCmdVel(cmdAirspeed);
-   //                  }
-   //                  break;
-
-   //      //-------------------------------------------------
-   //      // increase cmdAirspeed
-   //      //-------------------------------------------------
-   //      case 104:   if (cmdAirspeed < 400) {
-   //                     cmdAirspeed += 10;
-   //                     pR->setCmdVel(cmdAirspeed);
-   //                  }
-   //                  break;
-
-   //      //-------------------------------------------------
-   //      // decrease cmdAltitude
-   //      //-------------------------------------------------
-   //      case 105:   if (cmdAltitude > 1000) {
-   //                     cmdAltitude -= 500;
-   //                     pR->setCmdAlt(cmdAltitude);
-   //                  }
-   //                  break;
-
-   //      //-------------------------------------------------
-   //      // increase cmdAltitude
-   //      //-------------------------------------------------
-   //      case 106:   if (cmdAltitude < 40000) {
-   //                     cmdAltitude += 500;
-   //                     pR->setCmdAlt(cmdAltitude);
-   //                  }
-   //                  break;
-
-   //      //-------------------------------------------------
-   //      // decrease cmdHeading
-   //      //-------------------------------------------------
-   //      case 107:   cmdHeading -= 10;
-   //                  if (cmdHeading < -180.0) cmdHeading += 360;
-   //                     pR->setCmdHdg(cmdHeading);
-   //                  break;
-
-   //      //-------------------------------------------------
-   //      // increase cmdHeading
-   //      //-------------------------------------------------
-   //      case 108:   cmdHeading += 10;
-   //                  if (cmdHeading > 180.0) cmdHeading -= 360;
-   //                     pR->setCmdHdg(cmdHeading);
-   //                  break;
-
-   //      //-------------------------------------------------
-   //      case 111:   passiveEnable = true; 
-   //                  break;
-
-   //      case 112:   passiveEnable = false; 
-   //                  break;
-
-   //      default:    break;
-   //   }
-   //}
+   // cmdAirspeed, cmdAltitude, cmdHeading up, down
+   Simulation::Player* pA = getOwnship();
+   Simulation::Autopilot* ap = 0;
+   if (pA != 0) {
+      ap = (Simulation::Autopilot*)pA->getPilot();
+   }
+   if (page != 0 && ap != 0) {
+      if (b == DEC_RANGE) {
+         if (page->getRange() > 5) {
+            page->setRange(page->getRange() - 5);
+         }
+      }
+      else if (b == INC_RANGE) {
+         if (page->getRange() < 320) {
+            page->setRange(page->getRange() + 5);
+         }
+      }
+      else if (b == DEC_CMD_AS) {
+         double cmdAirspeed = ap->getCommandedVelocityKts();
+         if (cmdAirspeed > 100) {
+            cmdAirspeed -= 10;
+            ap->setCommandedVelocityKts(cmdAirspeed);
+         }      
+      }
+      else if (b == INC_CMD_AS) {
+         double cmdAirspeed = ap->getCommandedVelocityKts();
+         if (cmdAirspeed < 400) {
+            cmdAirspeed += 10;
+            ap->setCommandedVelocityKts(cmdAirspeed);
+         }
+      }
+      else if (b == DEC_CMD_ALT) {
+         double cmdAltitude = ap->getCommandedAltitudeFt();
+         if (cmdAltitude > 1000) {
+            cmdAltitude -= 500;
+            ap->setCommandedAltitudeFt(cmdAltitude);
+         }      
+      }
+      else if (b == INC_CMD_ALT) {
+         double cmdAltitude = ap->getCommandedAltitudeFt();
+         if (cmdAltitude < 40000) {
+            cmdAltitude += 500;
+            ap->setCommandedAltitudeFt(cmdAltitude);
+         }
+      }
+      else if (b == DEC_CMD_HDG) {
+         double cmdHeading = ap->getCommandedHeadingD();
+         cmdHeading -= 10;
+         if (cmdHeading < -180.0) cmdHeading += 360;
+         ap->setCommandedHeadingD(cmdHeading);
+      }
+      else if (b == INC_CMD_HDG) {
+         double cmdHeading = ap->getCommandedHeadingD();
+         cmdHeading += 10;
+         if (cmdHeading > 180.0) cmdHeading -= 360;
+         ap->setCommandedHeadingD(cmdHeading);
+      }
+      else if (b == INC_CMD_AS_NPS) {
+         double maxAccel = ap->getMaxVelAcc();
+         if (maxAccel < 20) maxAccel++;
+         ap->setMaxVelAccNps(maxAccel);
+      }
+      else if (b == DEC_CMD_AS_NPS) {
+         double maxAccel = ap->getMaxVelAcc();
+         if (maxAccel > 1) maxAccel--;
+         ap->setMaxVelAccNps(maxAccel);
+      }
+      // Climb rate in meters per second
+      else if (b == INC_CMD_ALT_MPS) {
+         double maxClimb = ap->getMaxClimbRate();
+         if (maxClimb < 100) maxClimb += 5;
+         ap->setMaxClimbRateMps(maxClimb);
+      }
+      else if (b == DEC_CMD_ALT_MPS) {
+         double maxClimb = ap->getMaxClimbRate();
+         if (maxClimb > 5) maxClimb -= 5;
+         ap->setMaxClimbRateMps(maxClimb);
+      }
+      // Turn rate in degrees per second
+      else if (b == INC_CMD_HDG_ROT) {
+         double maxTR = ap->getMaxTurnRate();
+         if (maxTR < 25) maxTR++;
+         ap->setMaxTurnRateDps(maxTR);
+      }
+      else if (b == DEC_CMD_HDG_ROT) {
+         double maxTR = ap->getMaxTurnRate();
+         if (maxTR > 0) maxTR--;
+         ap->setMaxTurnRateDps(maxTR);
+      }
+      // Max bank (degrees)
+      else if (b == INC_CMD_HDG_BNK) {
+         double maxBank = ap->getMaxBankAngle();
+         if (maxBank < 90) maxBank++;
+         ap->setMaxBankAngleDeg(maxBank);
+      }
+      else if (b == DEC_CMD_HDG_BNK) {
+         double maxBank = ap->getMaxBankAngle();
+         if (maxBank > 0) maxBank--;
+         ap->setMaxBankAngleDeg(maxBank);
+      }
+      else if (b == PASSIVE_ENABLE) {
+         passiveEnable = true; 
+      }
+      else if (b == PASSIVE_DISABLE) {
+         passiveEnable = false;
+      }
+      // get our autopilot mode and change it
+      else if (b == CHANGE_AP_MODE) {
+         // if off, go to nav
+         bool navMode = ap->isNavModeOn();
+         bool loiterMode = ap->isLoiterModeOn();
+         bool flMode = ap->isFollowTheLeadModeOn();
+         // if in nav mode, go to loiter mode
+         if (navMode) {
+            ap->setNavMode(false);
+            ap->setLoiterMode(true);
+         }
+         // if in loiter mode, go to follow the lead mode
+         else if (loiterMode) {
+            ap->setLoiterMode(false);
+            ap->setFollowTheLeadMode(true);
+         }
+         // if in follow the lead mode, turn off all modes
+         else if (flMode) {
+            ap->setFollowTheLeadMode(false);
+         }
+         // all modes off, go back to nav
+         else ap->setNavMode(true);
+      }
+   }
 }
 
 // ----------------------------------------------------------------------------
@@ -287,18 +330,46 @@ void MapDisplay::updateData(const LCreal dt)
       cmdRange = (int) page->getRange();
    }
 
-   //// get pointer to Aircraft data
-   //LaeroModel* pR = 0;
-   //Simulation::Aircraft* pA = (Simulation::Aircraft*)getOwnship();
-   //if (pA != 0) {
-   //   pR = (LaeroModel*) pA->getDynamicsModel();
-   //}
+   double cmdAirspeed = 0, cmdAltitude = 0, cmdHeading = 0;
+   bool apButtonsVis = false;
+   // pilot max
+   double maxAccel = 0, maxTurn = 0, maxBank = 0, maxClimb = 0;
+   // default to autopilot mode off
+   int apMode = 1;
+   Simulation::Aircraft* pA = (Simulation::Aircraft*)getOwnship();
+   if (pA != 0) {
+      Simulation::Autopilot* ap = (Simulation::Autopilot*)pA->getPilot();
+      if (ap != 0) {
+         // button visibility is based on autopilot being in NO modes
+         apButtonsVis = (ap->isNavModeOn() || ap->isLoiterModeOn() || ap->isFollowTheLeadModeOn());
+         cmdAirspeed = ap->getCommandedVelocityKts();
+         cmdAltitude = ap->getCommandedAltitudeFt();
+         cmdHeading = ap->getCommandedHeadingD();
+         if (ap->isNavModeOn()) apMode = 2;
+         else if (ap->isLoiterModeOn()) apMode = 3;
+         else if (ap->isFollowTheLeadModeOn()) apMode = 4;
+         maxAccel = ap->getMaxVelAcc();
+         maxTurn = ap->getMaxTurnRate();
+         maxBank = ap->getMaxBankAngle();
+         maxClimb = ap->getMaxClimbRate();
+      }
+   }
 
    // get pointer to LaeroModel data
    send("cmdRange",     UPDATE_VALUE, cmdRange,     cmdRangeSD);
    send("cmdAirspeed",  UPDATE_VALUE, cmdAirspeed,  cmdAirspeedSD);
    send("cmdAltitude",  UPDATE_VALUE, cmdAltitude,  cmdAltitudeSD);
    send("cmdHeading",   UPDATE_VALUE, cmdHeading,   cmdHeadingSD);
+   // Autopilot mode
+   send("apMode", SELECT, apMode, apModeSD);
+   // button visibility
+   send("apReqButtons", SET_VISIBILITY, !apButtonsVis, apReqButtonVisSD);
+   // maximums
+   send("cmdAccel",   UPDATE_VALUE, maxAccel,   maxAccelSD);
+   send("cmdClimb",   UPDATE_VALUE, maxClimb,   maxClimbSD);
+   send("cmdRot",   UPDATE_VALUE, maxTurn,   maxTurnSD);
+   send("cmdBank", UPDATE_VALUE, maxBank, maxBankSD);
+
 }
 
 //------------------------------------------------------------------------------
--- mainLaeroModel/MapDisplay.h
+++ mainLaeroModel/MapDisplay.h
@@ -36,20 +36,50 @@ public:
    virtual void updateData(const LCreal dt = 0.000000);
 
 private:
+   // button hit enumerations
+   enum {
+      DEC_RANGE = 101,           // Increase / decrease the map range (NM)
+      INC_RANGE,
+      DEC_CMD_AS,                // Increase / decrease the commanded airspeed (Kts - only works with no autopilot mode engaged)
+      INC_CMD_AS,
+      DEC_CMD_ALT,               // Increase / decrease the commanded altitude (Ft - only works with no autopilot mode engaged)
+      INC_CMD_ALT,
+      DEC_CMD_HDG,               // Increase / decrease the commanded heading (Degs - only works with no autopilot mode engaged) 
+      INC_CMD_HDG,
+      PASSIVE_ENABLE,
+      PASSIVE_DISABLE,
+      INC_CMD_AS_NPS,            // Increase / decrease our rate of acceleration 
+      DEC_CMD_AS_NPS,
+      INC_CMD_ALT_MPS,           // Increase / decrease our rate of climb / dive
+      DEC_CMD_ALT_MPS,
+      INC_CMD_HDG_ROT,           // Increase / decrease our rate of change of the bank angle
+      DEC_CMD_HDG_ROT,
+      INC_CMD_HDG_BNK,           // Increase / decrease our maximum bank angle during turns
+      DEC_CMD_HDG_BNK,
+      CHANGE_AP_MODE             // Change the autopilot mode (Nav/Loiter/FollowTheLead or None)
+   };
+
    SPtr<Simulation::Station> myStation;
 
    int startX;         // startX of our last mouse position
    int startY;         // startY of our last mouse position
    bool dragging;      // are we dragging the map?
 
-   double cmdAirspeed;
-   double cmdAltitude;
-   double cmdHeading;
-
    SendData cmdRangeSD;
    SendData cmdAirspeedSD;
    SendData cmdAltitudeSD;
    SendData cmdHeadingSD;
+
+   // Button visibility for buttons that require no autopilot mode - commanded alt, heading, and airspeed
+   SendData apReqButtonVisSD;
+   // readout of the mode the autopilot is in
+   SendData apModeSD;
+
+   // pilot limits
+   SendData maxAccelSD;
+   SendData maxClimbSD;
+   SendData maxBankSD;
+   SendData maxTurnSD;
 
    bool passiveEnable;
 };
