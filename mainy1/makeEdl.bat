4d87269121170bb97146e3ca6273c82fbce66911
Doug Hodson <doug@openeaagles.org>
2013-12-17 20:25:26 -0500

    updated edl creation

--- mainy1/makeEdl.bat
+++ mainy1/makeEdl.bat
@@ -2,5 +2,4 @@ rem
 call ..\makedefs.bat
 rem
 %mcpp% configs\test1.epp > test1.edl -I ..\data -I ..\shared-libs  -DFONT_PATH="\"../data/fonts/\""  -I ..\..\openeaagles\include  -DFONT_11_19_PATH="\"../data/fonts/11x19/normal/\"" -DTEXTURES_PATH="\"../data/textures/\""
-%mcpp% configs\recordTest.epp > recordTest.edl  -I ..\data -I ..\shared-libs  -DFONT_PATH="\"../data/fonts/\""  -I ..\..\openeaagles\include  -DFONT_11_19_PATH="\"../data/fonts/11x19/normal/\"" -DTEXTURES_PATH="\"../data/textures/\""
 
