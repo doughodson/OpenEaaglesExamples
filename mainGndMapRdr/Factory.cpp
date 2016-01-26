c2b750f473a0d9080024f23872392dc2ebdd6b6f
Doug Hodson <doug@openeaagles.org>
2016-01-26 18:46:15 -0500

    renamed 'Terrain' namespace usage to 'terrain' to align with oe

--- mainGndMapRdr/Factory.cpp
+++ mainGndMapRdr/Factory.cpp
@@ -39,7 +39,7 @@ basic::Object* Factory::createObj(const char* name)
     }
 
     if (obj == nullptr) obj = Simulation::Factory::createObj(name);
-    if (obj == nullptr) obj = Terrain::Factory::createObj(name);
+    if (obj == nullptr) obj = terrain::Factory::createObj(name);
     if (obj == nullptr) obj = BasicGL::Factory::createObj(name);
     if (obj == nullptr) obj = glut::Factory::createObj(name);
     if (obj == nullptr) obj = basic::Factory::createObj(name);
