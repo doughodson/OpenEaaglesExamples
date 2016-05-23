f4bcc105334c16856b78f9ebcac1925b392c90ff
Doug Hodson <doug@openeaagles.org>
2016-05-23 16:36:05 -0400

    removed vs2012 target

--- build/premake/premake5.lua
+++ build/premake/premake5.lua
@@ -2,8 +2,6 @@
 -- If premake command is not supplied an action (target compiler), exit!
 --
 -- Target of interest:
---     vs2010     (Visual Studio 2010)
---     vs2012     (Visual Studio 2012)
 --     vs2013     (Visual Studio 2013)
 --     vs2015     (Visual Studio 2015)
 --     codeblocks (Code::Blocks)
