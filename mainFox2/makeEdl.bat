70f3eff4a00f9e43065b2cb7487dbba018e3004f
Doug Hodson <doug@openeaagles.org>
2016-05-23 21:26:21 -0400

    updated mainFox2 Makefile and added makeEdl.bat

---
+++ mainFox2/makeEdl.bat
@@ -0,0 +1,8 @@
+rem
+call ..\makedefs.bat
+rem
+%mcpp% configs/foxdisplay.epp > foxdisplay.edl ^
+ -I ..\..\openeaagles\include\openeaagles ^
+ -I ..\shared\data ^
+ -DFONT_PATH="\"../shared/data/fonts/\"" ^
+ -DTEXTURES_PATH="\"../shared/data/textures/\""
