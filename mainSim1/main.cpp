809b5db8990ea24ee20507dc6a54b990d811be2a
Doug Hodson <doug@openeaagles.org>
2016-05-26 10:36:57 -0400

    combining mainSim1 and mainSim2, as the only difference is configuration files

--- mainSim1/main.cpp
+++ mainSim1/main.cpp
@@ -19,38 +19,33 @@
 #include <string>
 #include <cstdlib>
 
-namespace oe {
-namespace example {
-
 // background frame rate
 const unsigned int bgRate = 10;
 
-simulation::Station* station = nullptr;
-
-// our class factory
-base::Object* factory(const std::string& name)
+// class factory
+oe::base::Object* factory(const std::string& name)
 {
-   base::Object* obj = nullptr;
+   oe::base::Object* obj = nullptr;
 
    // example libraries
-   if (obj == nullptr) obj = xzmq::factory(name);
+   if (obj == nullptr) obj = oe::xzmq::factory(name);
 
    // framework libraries
-   if (obj == nullptr) obj = otw::factory(name);
-   if (obj == nullptr) obj = simulation::factory(name);
-   if (obj == nullptr) obj = models::factory(name);
-   if (obj == nullptr) obj = dis::factory(name);
-   if (obj == nullptr) obj = base::factory(name);
+   if (obj == nullptr) obj = oe::otw::factory(name);
+   if (obj == nullptr) obj = oe::simulation::factory(name);
+   if (obj == nullptr) obj = oe::models::factory(name);
+   if (obj == nullptr) obj = oe::dis::factory(name);
+   if (obj == nullptr) obj = oe::base::factory(name);
 
    return obj;
 }
 
 // station builder
-simulation::Station* builder(const std::string& filename)
+oe::simulation::Station* builder(const std::string& filename)
 {
    // read configuration file
    unsigned int num_errors = 0;
-   base::Object* obj = base::edl_parser(filename, factory, &num_errors);
+   oe::base::Object* obj = oe::base::edl_parser(filename, factory, &num_errors);
    if (num_errors > 0) {
       std::cerr << "File: " << filename << ", number of errors: " << num_errors << std::endl;
       std::exit(EXIT_FAILURE);
@@ -63,7 +58,7 @@ simulation::Station* builder(const std::string& filename)
    }
 
    // do we have a base::Pair, if so, point to object in Pair, not Pair itself
-   base::Pair* pair = dynamic_cast<base::Pair*>(obj);
+   oe::base::Pair* pair = dynamic_cast<oe::base::Pair*>(obj);
    if (pair != nullptr) {
       obj = pair->object();
       obj->ref();
@@ -71,7 +66,7 @@ simulation::Station* builder(const std::string& filename)
    }
 
    // try to cast to proper object, and check
-   simulation::Station* station = dynamic_cast<simulation::Station*>(obj);
+   oe::simulation::Station* station = dynamic_cast<oe::simulation::Station*>(obj);
    if (station == nullptr) {
       std::cerr << "Invalid configuration file!" << std::endl;
       std::exit(EXIT_FAILURE);
@@ -91,23 +86,23 @@ int main(int argc, char* argv[])
    }
 
    // build Station
-   station = builder(configFilename);
+   oe::simulation::Station* station = builder(configFilename);
 
    // send a reset event and frame sim once
-   station->event(base::Component::RESET_EVENT);
+   station->event(oe::base::Component::RESET_EVENT);
    station->tcFrame( static_cast<double>(1.0/static_cast<double>(station->getTimeCriticalRate())) );
 
    // create time critical thread
    station->createTimeCriticalProcess();
    // short pause to allow os to startup thread
-   base::msleep(2000);
+   oe::base::msleep(2000);
 
    // calc delta time for background thread
    const double dt = 1.0/static_cast<double>(bgRate);
 
    // system Time of Day
-   double simTime = 0.0;                               // Simulator time reference
-   const double startTime = base::getComputerTime();   // Time of day (sec) run started
+   double simTime = 0.0;                                   // Simulator time reference
+   const double startTime = oe::base::getComputerTime();   // Time of day (sec) run started
 
    int k = 0;
    std::cout << "Starting background main loop ..." << std::endl;
@@ -116,15 +111,15 @@ int main(int argc, char* argv[])
       // update background thread
       station->updateData( static_cast<double>(dt) );
 
-      simTime += dt;                             // time of next frame
-      const double timeNow = base::getComputerTime();  // time now
+      simTime += dt;                                       // time of next frame
+      const double timeNow = oe::base::getComputerTime();  // time now
       const double elapsedTime = timeNow - startTime;
       const double nextFrameStart = simTime - elapsedTime;
       const int sleepTime = static_cast<int>(nextFrameStart*1000.0);
 
       // wait for the next frame
       if (sleepTime > 0)
-         base::msleep(sleepTime);
+         oe::base::msleep(sleepTime);
 
       std::cout << ".";
       k += 1;
@@ -134,13 +129,4 @@ int main(int argc, char* argv[])
       }
    }
    return 0;
-}
-
-}
-}
-
-//
-int main(int argc, char* argv[])
-{
-   return oe::example::main(argc, argv);
 }
