a68fb511ed815502dfc3fe8d32c6031eca71c9bb
Doug Hodson <doug@openeaagles.org>
2016-05-26 10:58:51 -0400

    deleting mainSim2 example

--- mainSim2/makeEdl.bat
+++
@@ -1,7 +0,0 @@
-rem
-call ..\makedefs.bat
-rem
-%mcpp% configs/test1.epp > test1.edl -I ..\shared\data
-%mcpp% configs/test2.epp > test2.edl -I ..\shared\data
-%mcpp% configs/test3.epp > test3.edl -I ..\shared\data
-
