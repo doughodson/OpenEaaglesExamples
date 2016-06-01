6ed76e93e03b3e2a091b6841908db4ce7c747b99
Doug Hodson <doug@openeaagles.org>
2016-06-01 18:14:02 -0400

    deleted mainFox3 example

--- mainFox3/makeEdl.bat
+++
@@ -1,8 +0,0 @@
-rem
-call ..\makedefs.bat
-rem
-%mcpp% configs/foxstation.epp > foxstation.edl ^
- -I ..\..\openeaagles\include\openeaagles ^
- -I ..\shared\data ^
- -DFONT_PATH="\"../shared/data/fonts/\"" ^
- -DTEXTURES_PATH="\"../shared/data/textures/\""
