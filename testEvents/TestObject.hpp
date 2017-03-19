328e9c202df320f44ade529c6918cf92e3859b79
Doug Hodson <doug@openeaagles.org>
2017-03-19 02:34:14 -0400

    minor code formatting cleanup

--- testEvents/TestObject.hpp
+++ testEvents/TestObject.hpp
@@ -20,20 +20,20 @@ public:
    TestObject();
 
    // set functions
-   void setBoolean(const bool x)       { boolVal = x;     }
-   void setInteger(const int x)        { intVal = x;      }
-   void setFloat(const float x)        { floatVal = x;    }
-   void setDouble(const double x)      { doubleVal = x;   }
-   void setReal(const double x)        { realVal = x;     }
-   void setChar(const std::string& x)  { charVal = x;    }
+   void setBoolean(const bool x)       { boolVal = x;      }
+   void setInteger(const int x)        { intVal = x;       }
+   void setFloat(const float x)        { floatVal = x;     }
+   void setDouble(const double x)      { doubleVal = x;    }
+   void setReal(const double x)        { realVal = x;      }
+   void setChar(const std::string& x)  { charVal = x;      }
 
    // get functions
-   bool getBoolean()                   { return boolVal;         }
-   int getInteger()                    { return intVal;          }
-   float getFloat()                    { return floatVal;        }
-   double getDouble()                  { return doubleVal;       }
-   double getReal()                    { return realVal;         }
-   const std::string& getChar()        { return charVal; }
+   bool getBoolean()                   { return boolVal;   }
+   int getInteger()                    { return intVal;    }
+   float getFloat()                    { return floatVal;  }
+   double getDouble()                  { return doubleVal; }
+   double getReal()                    { return realVal;   }
+   const std::string& getChar()        { return charVal;   }
 
 private:
    bool boolVal {};
